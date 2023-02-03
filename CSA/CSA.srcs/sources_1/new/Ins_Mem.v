f10f49c2770af8d795c21250c44b4039845bcf74
CSA <abeywickramakcs.19@uom.lk>
2023-02-03 17:01:31 +0530

    Ins_Mem modified to handle MEM_SIZE changes

--- CSA/CSA.srcs/sources_1/new/Ins_Mem.v
+++ CSA/CSA.srcs/sources_1/new/Ins_Mem.v
@@ -1,36 +1,17 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.02.2023 10:15:46
-// Design Name: 
-// Module Name: Ins_Mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module Ins_Mem #(parameter WIDTH = 32) (
+module Ins_Mem #(parameter WIDTH = 32,parameter MEM_SIZE=5) (
     input [WIDTH-1:0] pc,
     output [WIDTH-1:0] ins
     );
 
-reg [WIDTH-1:0] mem [WIDTH-1:0];
+reg [WIDTH-1:0] mem [2**MEM_SIZE-1:0];
+wire  [MEM_SIZE-1:0] mem_addr=pc[MEM_SIZE-1:0];
 
 initial begin
 $readmemh("program.mem",mem);
 end
 
-assign ins=mem[pc];
+assign ins=mem[mem_addr];
     
 endmodule
