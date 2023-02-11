10ce9037904206184125ed7d1abcb27888addf19
SasininduSV <kssvikasitha1999@gmail.com>
2023-02-11 13:33:59 +0530

    new branch

--- hydra-merge/hydra-merge.srcs/sources_1/new/Data_Mem.v
+++ hydra-merge/hydra-merge.srcs/sources_1/new/Data_Mem.v
@@ -5,16 +5,14 @@ module Data_Mem  #(parameter WIDTH = 32,parameter MEM_SIZE=7) (
     output [WIDTH-1:0] test
 );
 
-reg [WIDTH-1:0] mem [2**MEM_SIZE-1:0];
-wire  [MEM_SIZE-1:0] mem_addr=addr[MEM_SIZE-1:0];
+reg [WIDTH-1:0] mem [2**MEM_SIZE-1:0][2**2-1:0];
+wire  [MEM_SIZE-1:0] mem_addr_row=addr[MEM_SIZE+3:4];
+wire  [1:0] mem_addr_col=addr[3:2];
 
-assign read_data=(mem_read)? mem[addr]: 0;
+assign read_data=(mem_read)? mem[mem_addr_row][mem_addr_col]: 0;
 
 always @(posedge clk) begin
     if(mem_write)
-        mem[mem_addr]<=write_data;
+        mem[mem_addr_row][mem_addr_col]<=write_data;
 end
-
-assign test=mem[92];
-
 endmodule
