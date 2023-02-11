984d9919198a84ee22918d52e9f18da14a0679f3
SasininduSV <kssvikasitha1999@gmail.com>
2023-02-12 00:13:57 +0530

    cache added

--- hydra-merge/hydra-merge.srcs/sources_1/new/Data_Mem.v
+++ hydra-merge/hydra-merge.srcs/sources_1/new/Data_Mem.v
@@ -1,12 +1,11 @@
 module Data_Mem  #(parameter WIDTH = 32,parameter MEM_SIZE=7) (
     input clk,mem_write,mem_read,
     input [WIDTH-1:0] addr,write_data,
-    output [WIDTH-1:0] read_data,
-    output [WIDTH-1:0] test
+    output [WIDTH-1:0] read_data
 );
 
 reg [WIDTH-1:0] mem [2**MEM_SIZE-1:0];
-wire  [MEM_SIZE-1:0] mem_addr=addr[MEM_SIZE-1:0];
+wire  [MEM_SIZE-1:0] mem_addr=addr[MEM_SIZE+1:2];
 
 assign read_data=(mem_read)? mem[addr]: 0;
 
@@ -15,6 +14,5 @@ always @(posedge clk) begin
         mem[mem_addr]<=write_data;
 end
 
-assign test=mem[92];
 
 endmodule
