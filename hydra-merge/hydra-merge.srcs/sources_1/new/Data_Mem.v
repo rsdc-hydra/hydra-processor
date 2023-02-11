08a6379f9e60c0fdc9eb29adbcfc36bfb2d748c1
SasininduSV <kssvikasitha1999@gmail.com>
2023-02-11 15:04:06 +0530

    allignment correction

--- hydra-merge/hydra-merge.srcs/sources_1/new/Data_Mem.v
+++ hydra-merge/hydra-merge.srcs/sources_1/new/Data_Mem.v
@@ -1,18 +1,18 @@
 module Data_Mem  #(parameter WIDTH = 32,parameter MEM_SIZE=7) (
-    input clk,mem_write,mem_read,
+    input clk,mem_write, mem_read,
     input [WIDTH-1:0] addr,
     input [(WIDTH*4)-1:0] write_data,
     output [(WIDTH*4)-1:0] read_data
 );
 
 reg [WIDTH-1:0] mem [2**MEM_SIZE-1:0][2**2-1:0];
-wire  [MEM_SIZE-1:0] mem_addr_row=addr[MEM_SIZE+3:4];
-wire  [1:0] mem_addr_col=addr[3:2];
+wire  [MEM_SIZE-1:0] mem_addr_row = addr[MEM_SIZE+3:4];
+wire  [1:0] mem_addr_col = addr[3:2];
 
-assign read_data=(mem_read)? mem[mem_addr_row][mem_addr_col]: 0;
+assign read_data = (mem_read)? mem[mem_addr_row][mem_addr_col]: 0;
 
 always @(posedge clk) begin
     if(mem_write)
-        mem[mem_addr_row][mem_addr_col]<=write_data;
+        mem[mem_addr_row] <= write_data;
 end
 endmodule
